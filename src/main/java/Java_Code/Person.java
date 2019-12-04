c3988c9512af743bd8117525e9fbbfa128727544
fatih birinci <fatihbirincim@gmail.com>
2019-12-04 17:31:54 -0500

    Added consructerto person class

--- src/main/java/Java_Code/Person.java
+++ src/main/java/Java_Code/Person.java
@@ -2,9 +2,14 @@ package Java_Code;
 
 public class Person {
 
-   private int age;
-    String havsiler =" tavaya";
-private double salary  = 5.34;
-   
-   private String  question = "when is the end of date";
+    private int age;
+    String havsiler = " tavaya";
+
+    private Person(int age){
+
+        this.age = age;
+
+
+        System.out.println("that one is last change");
+    }
 }
